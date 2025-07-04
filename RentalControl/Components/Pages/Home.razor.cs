c3b9156a64e176887e9266c19108f065f8a66919
Eduardo Flores <edfloreshz@gmail.com>
2025-07-03 20:05:02 -0700

    remove: blazor

--- RentalControl/Components/Pages/Home.razor.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace RentalControl.Components.Pages;
-
-public partial class Home : ComponentBase
-{
-}
--- RentalControl/Program.cs
+++ RentalControl/Program.cs
@@ -1,7 +1,5 @@
 using Carter;
-using MudBlazor.Services;
 using QuestPDF.Infrastructure;
-using RentalControl.Components;
 using RentalControl.Services;
 using Scalar.AspNetCore;
 using Supabase.Postgrest;
@@ -10,13 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 QuestPDF.Settings.License = LicenseType.Community;
 
-// Add services to the container.
 builder.Services.AddOpenApi();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
 builder.Services.AddMediator();
 builder.Services.AddCarter();
-builder.Services.AddMudServices();
 
 builder.Services.AddSingleton(new Client(builder.Configuration["Postgrest:Url"]!));
 builder.Services.AddSingleton<TenantService>();
@@ -26,11 +20,9 @@ builder.Services.AddSingleton<AddressService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -39,11 +31,6 @@ app.UseHttpsRedirection();
 app.MapOpenApi();
 app.MapScalarApiReference();
 
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
 
 app.MapCarter();
 
